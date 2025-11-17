bf23e10ca5937321a34e3b734b92444c541060b8
Ankit Kumar <nasaankit01@gmail.com>
2025-11-17 15:14:32 +0530

    removed main block

--- build_llm_from_scratch/multi_head_attention.py
+++ build_llm_from_scratch/multi_head_attention.py
@@ -63,32 +63,3 @@ class MultiHeadAttention(nn.Module):
         )
 
         return context_vectors
-
-
-if __name__ == "__main__":
-    inputs = torch.tensor(
-        [
-            [0.43, 0.15, 0.89],  # Your
-            [0.55, 0.87, 0.66],  # Journey
-            [0.57, 0.85, 0.64],  # Starts
-            [0.22, 0.58, 0.33],  # with
-            [0.77, 0.25, 0.10],  # one
-            [0.05, 0.80, 0.55],  # step
-        ]
-    )
-    batch = torch.stack((inputs, inputs), dim=0)
-    print(batch.shape)
-
-    context_length = batch.shape[1]
-    dim_in = inputs.shape[1]
-    dim_out = 6
-    num_heads = 2
-
-    mheadatten = MultiHeadAttention(
-        dim_in, dim_out, context_length, dropout=0.2, num_heads=2, qkv_bias=False
-    )
-
-    output = mheadatten(batch)
-
-    print(output.shape)
-    print(output)
