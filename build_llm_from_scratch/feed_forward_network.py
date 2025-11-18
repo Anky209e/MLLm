016e6a7a244fa3a3ab33fd02f358845c4fc73bf5
Ankit Kumar <nasaankit01@gmail.com>
2025-11-18 12:12:02 +0530

    feed forward network

---
+++ build_llm_from_scratch/feed_forward_network.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+from gelu import GELU
+
+
+class FeedForwardNeuralNetwork(nn.Module):
+    def __init__(self, embedding_size, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.layer1 = nn.Linear(embedding_size, 4 * embedding_size)
+        self.layer2 = nn.Linear(4 * embedding_size, embedding_size)
+        self.gelu = GELU()
+
+    def forward(self, x):
+        output = self.layer1(x)
+        output = self.gelu(output)
+        output = self.layer2(output)
+
+        return output
