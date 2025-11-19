74b041a5b8c04c29153af9e1732b1c87c663b3a5
Ankit Kumar <nasaankit01@gmail.com>
2025-11-19 19:30:59 +0530

    testing gpt model

--- build_llm_from_scratch/main.py
+++ build_llm_from_scratch/main.py
@@ -1,36 +1,35 @@
-from multi_head_attention import MultiHeadAttention
-from layer_normalisation import LayerNorm
+import tiktoken
 import torch
 
+from gpt import GPTModel
 
 if __name__ == "__main__":
-    inputs = torch.tensor(
-        [
-            [0.43, 0.15, 0.89],  # Your
-            [0.55, 0.87, 0.66],  # Journey
-            [0.57, 0.85, 0.64],  # Starts
-            [0.22, 0.58, 0.33],  # with
-            [0.77, 0.25, 0.10],  # one
-            [0.05, 0.80, 0.55],  # step
-        ]
-    )
-    batch = torch.stack((inputs, inputs), dim=0)
-    print(batch.shape)
+    tokenizer = tiktoken.get_encoding("gpt2")
+    batch = []
+    txt1 = "Every effort moves you"
+    txt2 = "Every day holds a"
+    batch.append(torch.tensor(tokenizer.encode(txt1)))
+    batch.append(torch.tensor(tokenizer.encode(txt2)))
+    batch = torch.stack(batch, dim=0)
+    print(batch)
+    GPT_CONFIG_124M = {
+        "vocab_size": 50257,
+        "context_length": 1024,
+        "embedding_dim": 768,
+        "n_heads": 12,  # no of attention heads
+        "n_layers": 12,  # no of transformer blocks
+        "drop_rate": 0.1,
+        "qkv_bias": False,
+    }
 
-    context_length = batch.shape[1]
-    dim_in = inputs.shape[1]
-    dim_out = 6
-    num_heads = 2
-    norm_layer = LayerNorm(embedding_dim=dim_out)
-    mheadatten = MultiHeadAttention(
-        dim_in, dim_out, context_length, dropout=0.2, num_heads=2, qkv_bias=False
-    )
-
-    output = mheadatten(batch)
-
-    print(output.shape)
-    print(output)
-
-    normed_output = norm_layer(output)
-    print(normed_output)
-    print(normed_output.var(dim=-1))
+    model = GPTModel(cfg=GPT_CONFIG_124M)
+    for i, j in model.named_parameters():
+        print(i, j.numel())
+    params = sum(p.numel() for p in model.parameters())
+    print(f"Total number of Parameters:{params:,}")
+    total_bytes = params * 4
+    total_size = total_bytes / (1024 * 1024)
+    print(f"Total Size of Model:{total_size:.2f}MB")
+    # out = model(batch)
+    # print(out.shape)
+    # print(out)
