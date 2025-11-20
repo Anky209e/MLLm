b9867ae3c97d3539c3d4781fc97de144e555ec54
Ankit Kumar <nasaankit01@gmail.com>
2025-11-20 12:50:53 +0530

    calculating train and val losses

--- build_llm_from_scratch/main.py
+++ build_llm_from_scratch/main.py
@@ -1,35 +1,88 @@
 import tiktoken
 import torch
+import os
 
 from gpt import GPTModel
-from utils import generate_text_simple, get_model_details
+from utils import (
+    generate_text_simple,
+    get_model_details,
+    calc_loss_loader,
+    calc_loss_batch,
+)
+from verdict_dataloader import VerdictDatasetV1, create_dataloader_v1
 
-if __name__ == "__main__":
-    tokenizer = tiktoken.get_encoding("gpt2")
-    batch = []
-    txt1 = "Every effort moves you"
-    txt2 = "Every day holds a"
-    batch.append(torch.tensor(tokenizer.encode(txt1)))
-    batch.append(torch.tensor(tokenizer.encode(txt2)))
-    batch = torch.stack(batch, dim=0)
-    print(batch)
-    GPT_CONFIG_124M = {
-        "vocab_size": 50257,
-        "context_length": 1024,
-        "embedding_dim": 768,
-        "n_heads": 12,  # no of attention heads
-        "n_layers": 12,  # no of transformer blocks
-        "drop_rate": 0.1,
-        "qkv_bias": False,
-    }
+FILE_PATH = "data.txt"
+TRAIN_RATION = 0.90
+BATCH_SIZE = 2
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Device:{DEVICE}")
 
-    model = GPTModel(cfg=GPT_CONFIG_124M)
-    params, total_size = get_model_details(model)
-    print(f"Total number of Parameters:{params:,}")
-    print(f"Total Size of Model:{total_size:.2f}MB")
-    # out = model(batch)
-    model.eval()
-    idx = generate_text_simple(model, batch, 6, GPT_CONFIG_124M["context_length"])
-    print(idx)
-    # print(out.shape)
-    # print(out)
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,  # Vocabulary size
+    "context_length": 256,  # Shortened context length (orig: 1024)
+    "embedding_dim": 768,  # Embedding dimension
+    "n_heads": 12,  # Number of attention heads
+    "n_layers": 12,  # Number of layers
+    "drop_rate": 0.1,  # Dropout rate
+    "qkv_bias": False,  # Query-key-value bias
+}
+
+with open(FILE_PATH, "r", encoding="utf-8") as f:
+    txt = f.read()
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+total_characters = len(txt)
+total_tokens = len(tokenizer.encode(txt))
+
+print("Characters:", total_characters)
+print("Tokens:", total_tokens)
+
+split_idx = int(TRAIN_RATION * len(txt))
+train_data = txt[:split_idx]
+val_data = txt[split_idx:]
+
+train_loader = create_dataloader_v1(
+    train_data,
+    batch_size=BATCH_SIZE,
+    max_length=GPT_CONFIG_124M["context_length"],
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=False,
+    shuffle=False,
+    num_workers=0,
+)
+
+val_loader = create_dataloader_v1(
+    val_data,
+    batch_size=BATCH_SIZE,
+    max_length=GPT_CONFIG_124M["context_length"],
+    stride=GPT_CONFIG_124M["context_length"],
+    drop_last=False,
+    shuffle=False,
+    num_workers=0,
+)
+
+# Sanity check
+if total_tokens * (TRAIN_RATION) < GPT_CONFIG_124M["context_length"]:
+    print(
+        "Not enough tokens for the training loader. "
+        "Try to lower the `GPT_CONFIG_124M['context_length']` or "
+        "increase the `training_ratio`"
+    )
+
+if total_tokens * (1 - TRAIN_RATION) < GPT_CONFIG_124M["context_length"]:
+    print(
+        "Not enough tokens for the validation loader. "
+        "Try to lower the `GPT_CONFIG_124M['context_length']` or "
+        "decrease the `training_ratio`"
+    )
+
+model = GPTModel(GPT_CONFIG_124M)
+model.to(device=DEVICE)
+
+with torch.no_grad():
+    train_loss = calc_loss_loader(train_loader, model, DEVICE)
+    val_loss = calc_loss_loader(val_loader, model, DEVICE)
+
+print(f"train_loss:{train_loss}")
+print(f"val_loss:{val_loss}")
