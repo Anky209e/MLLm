e08f4c519230eb6280020624928c776c82413710
Ankit Kumar <nasaankit01@gmail.com>
2025-11-21 12:58:02 +0530

    added spu support and generate with topk and temp

--- build_llm_from_scratch/utils.py
+++ build_llm_from_scratch/utils.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
 import logging
+import matplotlib.pyplot as plt
 import torch
 from matplotlib.ticker import MaxNLocator
 from tqdm import tqdm
@@ -23,9 +23,11 @@ def generate_text_simple(model, idx, max_new_tokens, context_size):
     return idx
 
 
-def text_to_token_ids(text, tokenizer):
+def text_to_token_ids(text, tokenizer, device):
     encoded = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
-    encoded_tensor = torch.tensor(encoded).unsqueeze(0)  # add batch dimension
+    encoded_tensor = (
+        torch.tensor(encoded).unsqueeze(0).to(device)
+    )  # add batch dimension
     return encoded_tensor
 
 
@@ -87,7 +89,7 @@ def evaluate_model(model, train_loader, val_loader, device, eval_iter):
 def generate_and_print_sample(model, tokenizer, device, start_context, set_train=True):
     model.eval()
     context_size = model.position_embeddings.weight.shape[0]
-    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+    encoded = text_to_token_ids(start_context, tokenizer, device=device)
     with torch.no_grad():
         token_ids = generate_text_simple(
             model=model, idx=encoded, max_new_tokens=50, context_size=context_size
@@ -96,6 +98,50 @@ def generate_and_print_sample(model, tokenizer, device, start_context, set_train
     print(decoded_text.replace("\n", " "))  # Compact print format
     if set_train:
         model.train()
+
+
+def generate(
+    model, idx, max_new_tokens, context_size, temperature=0.0, top_k=None, eos_id=None
+):
+    # For-loop is the same as before: Get logits, and only focus on last time step
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+
+        # New: Filter logits with top_k sampling
+        if top_k is not None:
+            # Keep only top_k values
+            top_logits, _ = torch.topk(logits, top_k)
+            min_val = top_logits[:, -1]
+            logits = torch.where(
+                logits < min_val, torch.tensor(float("-inf")).to(logits.device), logits
+            )
+
+        # New: Apply temperature scaling
+        if temperature > 0.0:
+            logits = logits / temperature
+
+            # Apply softmax to get probabilities
+            probs = torch.softmax(logits, dim=-1)  # (batch_size, context_len)
+
+            # Sample from the distribution
+            idx_next = torch.multinomial(probs, num_samples=1)  # (batch_size, 1)
+
+        # Otherwise same as before: get idx of the vocab entry with the highest logits value
+        else:
+            idx_next = torch.argmax(logits, dim=-1, keepdim=True)  # (batch_size, 1)
+
+        if (
+            idx_next == eos_id
+        ):  # Stop generating early if end-of-sequence token is encountered and eos_id is specified
+            break
+
+        # Same as before: append sampled index to the running sequence
+        idx = torch.cat((idx, idx_next), dim=1)  # (batch_size, num_tokens+1)
+
+    return idx
 
 
 def plot_losses(epochs_seen, tokens_seen, train_losses, val_losses):
@@ -132,6 +178,8 @@ def train_model_simple(
     start_context,
     tokenizer,
     device,
+    cfg,
+    writer=None,
 ):
     train_losses, val_losses, track_tokens_seen = [], [], []
     tokens_seen, global_step = 0, -1
@@ -156,6 +204,9 @@ def train_model_simple(
 
             pbar.set_postfix({"Loss": loss.item(), "Tokens Seen": tokens_seen})
 
+            if writer:
+                writer.add_scalar("Training Loss", loss.item(), global_step)
+                writer.add_scalar("Tokens Seen", tokens_seen, global_step)
             # Evaluation step:
         # if global_step % eval_freq == 0:
         train_loss, val_loss = evaluate_model(
@@ -176,6 +227,22 @@ def train_model_simple(
             f"Train Loss:{train_loss:.3f},Val Loss:{val_loss:.3f},"
             f"Tokens Seen:{tokens_seen}, Train Preplex:{round(train_perplexity.item())}"
         )
-        generate_and_print_sample(model, tokenizer, device, start_context)
-    torch.save(model.state_dict(), "weights.pth")
+        if writer:
+            writer.add_scalar("train_loss_epoch", train_loss, epoch)
+            writer.add_scalar("Validation_loss", val_loss, epoch)
+            writer.add_scalar("Perplixity", round(train_perplexity.item()), epoch)
+
+        token_ids = generate(
+            model=model,
+            idx=text_to_token_ids(start_context, tokenizer, device),
+            max_new_tokens=25,
+            context_size=cfg["context_length"],
+            top_k=50,
+            temperature=1,
+        )
+
+        generated_text = token_ids_to_text(token_ids, tokenizer)
+
+        print(generated_text)
+    torch.save(model.state_dict(), f"weights_{num_epochs}_256_768.pth")
     return train_losses, val_losses, track_tokens_seen
