2c020a48ecf5c5f6c49aad11333b1783a745ab6e
Ankit Kumar <nasaankit01@gmail.com>
2025-11-21 10:12:51 +0530

    training

--- build_llm_from_scratch/main.py
+++ build_llm_from_scratch/main.py
@@ -8,14 +8,19 @@ from utils import (
     get_model_details,
     calc_loss_loader,
     calc_loss_batch,
+    train_model_simple,
+    plot_losses,
 )
 from verdict_dataloader import VerdictDatasetV1, create_dataloader_v1
 
 FILE_PATH = "data.txt"
-TRAIN_RATION = 0.90
-BATCH_SIZE = 2
+FILE2 = "moby_dik.txt"
+
+TRAIN_RATIO = 0.90
+BATCH_SIZE = 6
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Device:{DEVICE}")
+start_context = "So close behind some promontory "
 
 GPT_CONFIG_124M = {
     "vocab_size": 50257,  # Vocabulary size
@@ -23,22 +28,24 @@ GPT_CONFIG_124M = {
     "embedding_dim": 768,  # Embedding dimension
     "n_heads": 12,  # Number of attention heads
     "n_layers": 12,  # Number of layers
-    "drop_rate": 0.1,  # Dropout rate
+    "drop_rate": 0.15,  # Dropout rate
     "qkv_bias": False,  # Query-key-value bias
 }
 
 with open(FILE_PATH, "r", encoding="utf-8") as f:
-    txt = f.read()
-
+    txt1 = f.read()
+with open(FILE2, "r", encoding="utf-8") as f:
+    txt2 = f.read()
+txt = txt1 + " <|endoftext|> " + txt2
 tokenizer = tiktoken.get_encoding("gpt2")
 
 total_characters = len(txt)
-total_tokens = len(tokenizer.encode(txt))
+total_tokens = len(tokenizer.encode(txt, allowed_special={"<|endoftext|>"}))
 
 print("Characters:", total_characters)
 print("Tokens:", total_tokens)
 
-split_idx = int(TRAIN_RATION * len(txt))
+split_idx = int(TRAIN_RATIO * len(txt))
 train_data = txt[:split_idx]
 val_data = txt[split_idx:]
 
@@ -63,14 +70,14 @@ val_loader = create_dataloader_v1(
 )
 
 # Sanity check
-if total_tokens * (TRAIN_RATION) < GPT_CONFIG_124M["context_length"]:
+if total_tokens * (TRAIN_RATIO) < GPT_CONFIG_124M["context_length"]:
     print(
         "Not enough tokens for the training loader. "
         "Try to lower the `GPT_CONFIG_124M['context_length']` or "
         "increase the `training_ratio`"
     )
 
-if total_tokens * (1 - TRAIN_RATION) < GPT_CONFIG_124M["context_length"]:
+if total_tokens * (1 - TRAIN_RATIO) < GPT_CONFIG_124M["context_length"]:
     print(
         "Not enough tokens for the validation loader. "
         "Try to lower the `GPT_CONFIG_124M['context_length']` or "
@@ -79,10 +86,24 @@ if total_tokens * (1 - TRAIN_RATION) < GPT_CONFIG_124M["context_length"]:
 
 model = GPTModel(GPT_CONFIG_124M)
 model.to(device=DEVICE)
+params, size = get_model_details(model)
+print(f"Total Params:{params:,}")
+print(f"Total Size:{size:.2f}MB")
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.0004, weight_decay=0.1)
+num_epochs = 100
 
-with torch.no_grad():
-    train_loss = calc_loss_loader(train_loader, model, DEVICE)
-    val_loss = calc_loss_loader(val_loader, model, DEVICE)
+train_losses, val_losses, tokens_seen = train_model_simple(
+    model=model,
+    train_loader=train_loader,
+    val_loader=val_loader,
+    optimizer=optimizer,
+    num_epochs=num_epochs,
+    eval_freq=10,
+    eval_iter=10,
+    start_context=start_context,
+    tokenizer=tokenizer,
+    device=DEVICE,
+)
 
-print(f"train_loss:{train_loss}")
-print(f"val_loss:{val_loss}")
+epochs_tensor = torch.linspace(0, num_epochs, len(train_losses))
+plot_losses(epochs_tensor, tokens_seen, train_losses, val_losses)
