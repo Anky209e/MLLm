19fc5b2efc95e8dfd76e09565ce3ead261a2a63b
Ankit Kumar <nasaankit01@gmail.com>
2025-12-29 18:18:10 +0530

    changed to sequential

--- build_llm_from_scratch/feed_forward_network.py
+++ build_llm_from_scratch/feed_forward_network.py
@@ -5,13 +5,13 @@ from gelu import GELU
 class FeedForwardNeuralNetwork(nn.Module):
     def __init__(self, embedding_dim, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.layer1 = nn.Linear(embedding_dim, 4 * embedding_dim)
-        self.layer2 = nn.Linear(4 * embedding_dim, embedding_dim)
-        self.gelu = GELU()
+        self.layers = nn.Sequential(
+            nn.Linear(embedding_dim, 4 * embedding_dim),
+            GELU(),
+            nn.Linear(4 * embedding_dim, embedding_dim),
+        )
 
     def forward(self, x):
-        output = self.layer1(x)
-        output = self.gelu(output)
-        output = self.layer2(output)
+        output = self.layers(x)
 
         return output
