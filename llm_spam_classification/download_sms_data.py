c5e4f5e30182cc3f4c7d3f64e7b4fb943999f309
Ankit Kumar <nasaankit01@gmail.com>
2026-01-02 16:43:09 +0530

    added data processing

--- llm_spam_classification/download_sms_data.py
+++ llm_spam_classification/download_sms_data.py
@@ -3,6 +3,7 @@ import ssl
 import zipfile
 import os
 from pathlib import Path
+import pandas as pd
 
 
 def download_and_unzip(url, zip_path, extracted_path, data_path):
@@ -26,6 +27,24 @@ def download_and_unzip(url, zip_path, extracted_path, data_path):
     print(f"File downloaded and saved as {data_path}")
 
 
+def create_balanced_dataset(df):
+    num_spam = df[df["Label"] == "spam"].shape[0]
+    ham_subset = df[df["Label"] == "ham"].sample(num_spam, random_state=2929)
+    balanced_df = pd.concat([ham_subset, df[df["Label"] == "spam"]])
+    balanced_df["Label"] = balanced_df["Label"].map({"ham": 0, "spam": 1})
+    return balanced_df
+
+
+def random_split(df, train_frac, vaildation_frac):
+    df = df.sample(frac=1, random_state=2929).reset_index(drop=True)
+    train_end = int(len(df) * train_frac)
+    validation_end = train_end + int(len(df) * vaildation_frac)
+    train_df = df[:train_end]
+    validation_df = df[train_end:validation_end]
+    test_df = df[validation_end:]
+    return train_df, validation_df, test_df
+
+
 if __name__ == "__main__":
     url = "https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip"
     zip_path = "sms_data.zip"
@@ -36,3 +55,13 @@ if __name__ == "__main__":
     download_and_unzip(
         url=url, zip_path=zip_path, extracted_path=extracted_path, data_path=data_path
     )
+    df = pd.read_csv(data_path, sep="\t", header=None, names=["Label", "Text"])
+    print(df.head(7))
+    balanced_df = create_balanced_dataset(df)
+    train_df, validation_df, test_df = random_split(balanced_df, 0.7, 0.1)
+    print(len(train_df))
+    print(len(validation_df))
+    print(len(test_df))
+    train_df.to_csv("sms_data/train.csv", index=None)
+    validation_df.to_csv("sms_data/validation.csv", index=None)
+    test_df.to_csv("sms_data/test.csv", index=None)
